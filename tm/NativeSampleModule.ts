318c72049fcef2d2302cf9e6d939147b1d1fe5ca
Wahid-Abdul <awahidag@gmail.com>
2022-12-20 17:31:51 +0530

    inital working commit

---
+++ tm/NativeSampleModule.ts
@@ -0,0 +1,11 @@
+import type {TurboModule} from 'react-native/Libraries/TurboModule/RCTExport';
+// import type {TurboModule} from 'react-native'; in future versions
+import {TurboModuleRegistry} from 'react-native';
+
+export interface Spec extends TurboModule {
+  readonly reverseString: (input: string) => string;
+}
+
+export default TurboModuleRegistry.getEnforcing<Spec>(
+  'NativeSampleModule',
+);
